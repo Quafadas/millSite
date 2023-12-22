615cd1ef8477b539ba5a1656d8b0ad3f24b494a6
Simon Parten <quafadas@gmail.com>
2023-12-22 10:21:07 +0100

    Update website

--- site/src/millSite/site.plugin.scala
+++ site/src/millSite/site.plugin.scala
@@ -1,15 +1,16 @@
 package millSite
 
-object UserApi {
-
   /**
    *
    * See 'Doc'
    *
    * We have to trick scalas static site generator, into thinkingt hat there is actual source code in the project.
    *
-   * However, as mill itself is 2.13, we can't compile the _actual_ plugin. So this is a dummy.
+   * However, as mill itself is on 2.13, we can't compile the _actual_ plugin and show the API here.
    *
     */
+object UserApi {
+
+
 }
 
